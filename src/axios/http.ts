9eae5ecc5a5c6832c6f990195085ec6af53c5ecb
曹磊 <ethan.cao@lianwei.com.cn>
2021-06-16 11:50:46 +0800

    完善

--- src/axios/http.ts
+++ src/axios/http.ts
@@ -1,10 +1,5 @@
 import axios from 'axios'
-import { serialize } from '../utils/serialize'
 import type { HttpMethodName, AxiosRequestConfig, CreateHttp, Http } from './types'
-
-// const DEFAULT_CONFIG: AxiosRequestConfig = {
-//   timeout: 15 * 1000
-// }
 
 // for loop create httpMethod
 const methodName: HttpMethodName[] = [ 'get', 'post', 'put', 'delete' ]
@@ -14,21 +9,21 @@ const errorHandler = (err: any) => Promise.reject(err)
 
 /**
  * a method which can create a object,
- * its has four properties (get, post, put, delete)
+ * with four properties (get, post, put, delete)
  */
-export const createHttp: CreateHttp = (props = {
-  baseURL: '/',
-  clearGetCache: false,
-  defaultConfig: {
-    timeout: 15 * 1000
-  },
-  requset: config => config,
-  response: response => response,
-}) => {
-  const { baseURL: baseurl, clearGetCache, defaultConfig, requset, response } = props
+export const createHttp: CreateHttp = (props = {}) => {
+  const {
+    baseURL: URL = '/',
+    clearGetCache = false,
+    defaultConfig = {
+      timeout: 15 * 1000
+    },
+    requset = config => config,
+    response = response => response
+  } = props
   const http = {} 
   methodName.forEach(method => {
-    (http as Http)[method] = (data, url, baseURL = baseurl) => {
+    (http as Http)[method] = (url, data = {}, baseURL = URL) => {
       // 1. create axios instance
       const instance = axios.create(defaultConfig)
   
@@ -41,11 +36,8 @@ export const createHttp: CreateHttp = (props = {
       // handle "data"(property)
       if (method === 'get') {
         requestData.params = clearGetCache ? { ...data, ts: Date.now() } : data
-      } else if (data instanceof FormData) {
+      } else if (method === 'post') {
         requestData.data = data
-      } else {
-        // transform data({ a: 1, b: 2 }) => ?a=1&b=2 
-        requestData.data = serialize(data)
       }
 
       // 4. send request
--- src/axios/types.ts
+++ src/axios/types.ts
@@ -1,25 +1,37 @@
 import type { AxiosRequestConfig as _AxiosRequestConfig, AxiosResponse } from 'axios'
 
-// axios default request params config, add ts to clear get's cache
+/**
+ * axios default request params config,
+ * add ts to clear get's cache
+ */
 export type AxiosRequestConfig = {
   ts?: number // tiemstamp
 } & _AxiosRequestConfig
 
 export type HttpMethodName = 'get' | 'post' | 'put' | 'delete'
+
 /**
  * every method have three properties
  * data: will in request body or request head
  * baseURL & url: to be final requset url
+ * 
  * why should i push baseURL in the last, because sometimes maybe u should change baseURL
  * in vuejs, we always instead it with proxy, and default value like '/' 
  * 
  * in this type, use P determines the return value of Promise
  * when we use this method, T can determines the type of data  
  */
-export type HttpMethod <P = any> = <T extends object> (data: T, url: string, baseURL?: string) => Promise<P>
+export type HttpMethod <P = any> = (url: string, data?: object, baseURL?: string) => Promise<P>
 
-// http will have 4 methods, get post put delete
+/**
+ * http will have 4 methods, get post put delete
+ */
 export type Http <P = any> = Record<HttpMethodName, HttpMethod<P>>
+
+/**
+ * interceptors type
+ */
+export type Axiosinterceptor <T> = (value: T) => T | Promise<T>
 
 /**
  * pick baseURL and interceptors for personal setting
@@ -28,16 +40,22 @@ export type Http <P = any> = Record<HttpMethodName, HttpMethod<P>>
  * baseURL was brought up seprately,because it will be statement as a global variable,
  * also it can be modify in every httpMethod
  * 
- * clearGetCache decide if add timestamp property to clear  get's cache
+ * clearGetCache decide if add timestamp property to clear get's cache
  * 
- * requset & response are interceptor of axios requset
+ * requset & response => interceptors
  */
 export type CreateHttpProps = {
   baseURL?: string
   clearGetCache?: boolean
   defaultConfig?: Omit<AxiosRequestConfig, 'baseURL'>
-  requset?: (value: AxiosRequestConfig) => AxiosRequestConfig | Promise<AxiosRequestConfig>
-  response?: (value: AxiosResponse) => AxiosResponse | Promise<AxiosResponse>
+  requset?: Axiosinterceptor<AxiosRequestConfig>
+  response?: Axiosinterceptor<AxiosResponse>
 }
-// type of createHttp
+
+/**
+ * method createHttp's type
+ * 
+ * when the method createHttp is called,
+ * P determines the type of result, or use default value => any
+ */
 export type CreateHttp = <P = any> (props?: CreateHttpProps) => Http <P>
